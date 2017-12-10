08516537a19f0c2c78b6e04ef26282d2f049be21
PeterChauYEG <peter.chau.esq@gmail.com>
2017-12-10 11:56:58 -0700

    init commit

--- Autobed-leveling/Marlin/Configuration.h
+++ Autobed-leveling/Marlin/Configuration.h
@@ -383,7 +383,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
   // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
   #define X_PROBE_OFFSET_FROM_EXTRUDER 50
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 6
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -2.3
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -1.66
 
   #define Z_RAISE_BEFORE_HOMING 4       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
---
+++ README.md
@@ -0,0 +1,6 @@
+# Prusa i3 Marlin Firmware
+This repo holds firmare configurations for my 3d printer.
+
+Default is the firmware which the printer shipped with.
+
+Autobed-leveling is the Default firmware with autobed leveling enabled via induction sensor.
